9f41674c36e9f27500fb9298920abe81a9e3fc0e
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-29 14:30:06 +0700

    1 file modified

---
+++ components/Tabbar.vue
@@ -0,0 +1,48 @@
+
+
+<template>
+
+	<div class="flex diygw-col-24 diygw-bottom">
+					<div class="diygw-grid diygw-actions">
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="font-size: 22px">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/1-001.png" />
+								</div>
+								<div class="diygw-grid-title">หน้าหลัก</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius diy-icon-hongbao" style="font-size:22px color:#ff3030;"></div>
+								<div class="diygw-grid-title">คูปอง</div>
+							</div>
+						</button>
+						<button class="diygw-action" style="background-color: rgba(255, 255, 255, 0) !important">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="font-size: 22px">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/2-001.png" />
+								</div>
+								<div class="diygw-grid-title">กิจกรรม</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius diy-icon-mark" style="font-size:22px color:#000000;">
+									<div class="diygw-tag badge bar5-badge-3 gradual-red">1</div>
+								</div>
+								<div class="diygw-grid-title">แชท</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="font-size: 22px">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/4-001.png" />
+								</div>
+								<div class="diygw-grid-title">ข้อมูล</div>
+							</div>
+						</button>
+					</div>
+				</div>
+
+      </template>
