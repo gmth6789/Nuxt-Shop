6b4a58bfe09964680c70cb3aa820f8a18125447d
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-29 14:32:55 +0700

    1 file modified

--- components/Tabbar.vue
+++ components/Tabbar.vue
@@ -7,7 +7,7 @@
 						<button class="diygw-action">
 							<div class="diygw-grid-inner">
 								<div class="diygw-grid-icon diygw-avatar" style="font-size: 22px">
-									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/1-001.png" />
+									<img mode="aspectFit" class="diygw-avatar-img" src="~/assets/1-001.png" />
 								</div>
 								<div class="diygw-grid-title">หน้าหลัก</div>
 							</div>
@@ -21,7 +21,7 @@
 						<button class="diygw-action" style="background-color: rgba(255, 255, 255, 0) !important">
 							<div class="diygw-grid-inner">
 								<div class="diygw-grid-icon diygw-avatar" style="font-size: 22px">
-									<img mode="aspectFit" class="diygw-avatar-img" src="./static/2-001.png" />
+									<img mode="aspectFit" class="diygw-avatar-img" src="~/assets/2-001.png" />
 								</div>
 								<div class="diygw-grid-title">กิจกรรม</div>
 							</div>
@@ -37,7 +37,7 @@
 						<button class="diygw-action">
 							<div class="diygw-grid-inner">
 								<div class="diygw-grid-icon diygw-avatar" style="font-size: 22px">
-									<img mode="aspectFit" class="diygw-avatar-img" src="./static/4-001.png" />
+									<img mode="aspectFit" class="diygw-avatar-img" src="~/assets/4-001.png" />
 								</div>
 								<div class="diygw-grid-title">ข้อมูล</div>
 							</div>
